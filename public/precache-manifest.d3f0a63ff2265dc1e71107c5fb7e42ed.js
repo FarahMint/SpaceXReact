e9138170b598a7e688edf9b01bd2d3a48cb9c5d4
FarahMint <zfarah.el@gmail.com>
2018-12-11 13:30:48 +0300

    first react files

---
+++ public/precache-manifest.d3f0a63ff2265dc1e71107c5fb7e42ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "33051480c3ac205b2a45",
+    "url": "/static/js/main.33051480.chunk.js"
+  },
+  {
+    "revision": "9eb111db63cabe2e5129",
+    "url": "/static/js/1.9eb111db.chunk.js"
+  },
+  {
+    "revision": "33051480c3ac205b2a45",
+    "url": "/static/css/main.aa8d3c2f.chunk.css"
+  },
+  {
+    "revision": "b2c8500546154534092681ec8cdd7c29",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
-
+const cors = require("cors");
 const app = express();
+const path = require("path");
+// allow cross-origin
+app.use(cors());
 
 app.use(
   "/graphql",
@@ -11,6 +14,13 @@ app.use(
     graphiql: true
   })
 );
+
+//  create static folder
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
